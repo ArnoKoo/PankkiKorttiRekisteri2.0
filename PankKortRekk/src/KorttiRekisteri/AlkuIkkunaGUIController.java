fc53ec9240af84c7d112fe2df84a406064280ad6
ArnoKoo <arno.korhonen05@gmail.com>
2025-02-17 16:43:52 +0200

    GUIController, GUIView ja Main päivitys

--- PankKortRekk/src/KorttiRekisteri/AlkuIkkunaGUIController.java
+++ PankKortRekk/src/KorttiRekisteri/AlkuIkkunaGUIController.java
@@ -1,4 +1,8 @@
 package KorttiRekisteri;
+import fi.jyu.mit.fxgui.ModalController;
+import fi.jyu.mit.fxgui.ModalControllerInterface;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 //import fi.jyu.mit.fxgui.Dialogs;
 
@@ -6,9 +10,33 @@ package KorttiRekisteri;
  * @author OMISTAJA
  * @version 9.2.2025
  *
- */
-public class AlkuIkkunaGUIController {
-    //private void JokuVitunErrori() {
-        //Dialogs.showMessageDialog("Pankkianne ei ole rekisterissä!");
-    //}
+ */ 
+public class AlkuIkkunaGUIController implements ModalControllerInterface <String>{
+    @FXML private void SiirryPaaSivulle() {
+        PaaikkunaGUIController.alkuNaytto(null, "Muokkaa jäsenen tietoja");
+    }
+    
+    @FXML private Button suljeNappi;
+    
+    public void Lopeta() {
+        ModalController.closeStage(suljeNappi);
+    }
+
+    @Override
+    public String getResult() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void handleShown() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setDefault(String arg0) {
+        // TODO Auto-generated method stub
+        
+    }
 }
--- PankKortRekk/src/KorttiRekisteri/PaaikkunaGUIController.java
+++ PankKortRekk/src/KorttiRekisteri/PaaikkunaGUIController.java
@@ -1,10 +1,79 @@
 package KorttiRekisteri;
 
+import fi.jyu.mit.fxgui.Dialogs;
+import fi.jyu.mit.fxgui.ModalController;
+import fi.jyu.mit.fxgui.ModalControllerInterface;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
 /**
  * @author OMISTAJA
  * @version 9.2.2025
  *
  */
-public class PaaikkunaGUIController {
-      // TODO
+public class PaaikkunaGUIController implements ModalControllerInterface <String> {
+    @FXML private void TestiPesti() {
+        Dialogs.showMessageDialog("Ei osata viel :D");
+    }
+    
+    @FXML private void Apua() {
+        Dialogs.showMessageDialog("You're on your own lil bro.");
+    }
+    
+    @FXML private void Tietoja() {
+        Dialogs.showMessageDialog("Kunpa tietäisin itekki.");
+    }
+    
+    @FXML private void Lopeta() {
+        Platform.exit();
+    }
+    
+    //Jäsen muokkaukset
+    @FXML private void handleMuokkaJasen() {
+        MuokkaJasenGUIController.alkuNaytto(null, "Muokkaa jäsenen tietoja"); //toimii
+    }
+    
+    @FXML private void handleLisaaJasen() {
+        LisaaJasenGUIController.alkuNaytto(null, "Lisää uusi jäsen"); //toimii 
+    }
+    
+    @FXML private void handlePoistaJasen() {
+        PoistaJasenGUIController.alkuNaytto(null, "Poista jäsen"); //toimii
+    }
+    
+    //Pankkikortti muokkaukset
+    @FXML private void handleLisaaPankkiKortti() {
+        LisaaKorttiGUIController.alkuNaytto(null, "Lisää uusi pankkikortti"); //toimii
+    }
+    
+    @FXML private void handleMuokkaaPankkiKorttiVali() {
+        MuokkaaGUIController.alkuNaytto(null, "Muokkaa pankkikortin tietoja"); //toimii
+    }
+    
+    @FXML private void handlePoistaPankkiKortti() {
+        PoistaPankkiKorttiGUIController.alkuNaytto(null, "Poista pankkikortti"); //toimii
+    }
+    
+    public static String alkuNaytto(Stage modalityStage, String oletus) {
+        return ModalController.showModal(MuokkaJasenGUIController.class.getResource("PaaikkunaGUIView.fxml"), "Tervetuloa AgoBank:iin!", modalityStage, oletus);
+    }
+
+    @Override
+    public String getResult() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void handleShown() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setDefault(String oletus) {
+        // TODO Auto-generated method stub
+        
+    }
 }
